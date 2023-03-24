a395520d9b048d26c14347ce3f47d510ddd4ef10
SachinkumarSonu <127083841+SachinkumarSonu@users.noreply.github.com>
2023-03-24 11:22:17 +0530

    Add files via upload

---
+++ airpods.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-airpods',
+  templateUrl: './airpods.component.html',
+  styleUrls: ['./airpods.component.css']
+})
+export class AirpodsComponent {
+
+}
